ddfd6826971faefd80bd2fafbb518177b7d9183f
Kevin O'Brien <kdobrien22@gmail.com>
2022-03-22 14:39:00 -0700

    Finished part 3.  Seems to be some issues where fields are printing as [Object] not sure if working as intended.

--- db/seed.js
+++ db/seed.js
@@ -7,12 +7,17 @@ const {
   updatePost,
   getAllPosts,
   getPostsByUser,
+  addTagsToPost,
+  createTags,
   getUserById,
+  getPostsByTagName,
 } = require("./index");
 
 async function dropTables() {
   try {
     await client.query(`
+        DROP TABLE IF EXISTS post_tags;
+        DROP TABLE IF EXISTS tags;
         DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
         `);
@@ -43,10 +48,43 @@ async function createTables() {
             content TEXT NOT NULL,
             active BOOLEAN DEFAULT true
         );
+        CREATE TABLE tags(
+            id SERIAL PRIMARY KEY,
+            name varchar(255) UNIQUE NOT NULL
+        );
+        CREATE TABLE post_tags(
+            "postId" INTEGER REFERENCES posts(id),
+            "tagId" INTEGER REFERENCES tags(id),
+            CONSTRAINT UC_tag UNIQUE ("postId", "tagId")    
+        );
         `);
     console.log("Finished building tables!");
   } catch (error) {
     console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      "#happy",
+      "#worst-day-ever",
+      "#youcandoanything",
+      "#catmandoeverything",
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
     throw error;
   }
 }
@@ -105,20 +143,21 @@ async function createInitialPosts() {
       title: "First Post",
       content:
         "This is my first post. I hope I love writing blogs as much as I love reading them.",
+      tags: ["#happy", "#youcandoanything"],
     });
 
     await createPost({
       authorId: sandra.id,
-      title: "First Post",
-      content:
-        "This is my first post. I hope I love writing blogs as much as I love reading them.",
+      title: "How does this work?",
+      content: "Seriously, does this even do anything?",
+      tags: ["#happy", "#worst-day-ever"],
     });
 
     await createPost({
       authorId: glamgal.id,
-      title: "First Post",
-      content:
-        "This is my first post. I hope I love writing blogs as much as I love reading them.",
+      title: "Living the Glam Life",
+      content: "Do you even? I swear that half of you are posing.",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
     });
   } catch (error) {
     throw error;
@@ -151,9 +190,19 @@ async function testDB() {
     });
     console.log("Result:", updatePostResult);
 
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"],
+    });
+    console.log("Result:", updatePostTagsResult);
+
     console.log("Calling getUserById with 1");
     const albert = await getUserById(1);
     console.log("Result:", albert);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
